b70cefad1fdc2a77eb0ae37e1b6d4e57a50e221f
JangYoungHun <5035889@gmail.com>
2021-09-05 13:04:59 +0900

    add_solved_problem

---
+++ Java/javaPractice/javaPrac/src/study/LIS_Algorithm1.java
@@ -0,0 +1,22 @@
+package study;
+
+//¿ÏÀüÅ½»ö O(N^2)
+public class LIS_Algorithm1 {
+
+	public static void main(String[] args) {
+		
+		int arr[] = { 6, 2, 5, 1, 7, 4, 8, 3};
+		int n = arr.length;
+		int dp[] = new int[n+1];
+		
+		for(int i =0; i<n; i++) {		
+			dp[i] = 1;
+			for(int j =0; j<i; j++) {
+				if(arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j]+1);
+				}
+			}
+		}
+	}
+
+}
---
+++ Java/javaPractice/javaPrac/src/study/LIS_Algorithm2.java
@@ -0,0 +1,48 @@
+package study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//완전탑색 O(N^2)
+public class LIS_Algorithm2 {
+
+	public static void main(String[] args) {
+		
+		int arr[] = { 4,7,1,2,3,4,0,8};
+		int n = arr.length;
+		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(0);
+		
+		for(int i =0; i<n; i++) {
+			int value = list.get(list.size()-1);
+			if(value < arr[i]) {
+				list.add(arr[i]);
+			}
+			else {
+				int index = binarySearch(0, list.size()-1, arr[i], list);
+				index = (index < 0) ? (index+1)*(-1) : index;
+				list.set(index, arr[i]);
+			}
+				
+		}
+}
+	
+	static int binarySearch(int left, int right, int num, List<Integer> list) {
+		
+		while(left <= right) {
+			int p = (left+right)/2;
+			if(list.get(p) == num) {
+				return p;
+			}
+			else if(list.get(p)  < num) {
+				left = p+1;				
+			}
+			else {
+				right = p-1;
+			}			
+		}
+			return (-1)*(left+1);
+	}
+	
+}
